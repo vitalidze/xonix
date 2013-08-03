14accc60d302fe483aa032f32c8e1c6aba5c4c3e
Vitaly Litvak <vitavaque@gmail.com>
2013-08-03 09:22:47 +0400

    Main class cleanup

--- src/su/litvak/xonix/Main.java
+++ src/su/litvak/xonix/Main.java
@@ -1,9 +1,5 @@
 package su.litvak.xonix;
 
-import java.awt.*;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Created with IntelliJ IDEA.
  * User: Vitaly
@@ -13,20 +9,5 @@ import java.util.Set;
  */
 public class Main {
     public static void main(String[] args) {
-        Field f = new Field(5, 5);
-
-        System.out.println(f);
-
-        Set<Point> path = new HashSet<Point>();
-        path.add(new Point(1, 2));
-        path.add(new Point(2, 2));
-        path.add(new Point(3, 2));
-        path.add(new Point(3, 3));
-        path.add(new Point(3, 4));
-        path.add(new Point(2, 4));
-        path.add(new Point(1, 4));
-        f.cut(path);
-
-        System.out.println(f);
     }
 }
