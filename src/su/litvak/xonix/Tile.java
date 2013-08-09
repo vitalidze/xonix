7a993389ded6548d00f3e9e97567de989e4de9c8
Vitaly Litvak <vitavaque@gmail.com>
2013-08-10 01:40:41 +0400

    Added tile color, path tile

--- src/su/litvak/xonix/Tile.java
+++ src/su/litvak/xonix/Tile.java
@@ -1,5 +1,7 @@
 package su.litvak.xonix;
 
+import java.awt.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Vitaly
@@ -8,11 +10,17 @@ package su.litvak.xonix;
  * To change this template use File | Settings | File Templates.
  */
 public enum Tile {
-    EARTH('+'), WATER('O'), HERO('H'), ENEMY('E');
+    EARTH('+', Color.YELLOW),
+    WATER('O', Color.BLUE),
+    HERO('H', Color.GREEN),
+    ENEMY('E', Color.RED),
+    PATH('x', Color.BLACK);
 
     final char symbol;
+    final Color color;
 
-    private Tile(char symbol) {
+    private Tile(char symbol, Color color) {
         this.symbol = symbol;
+        this.color = color;
     }
 }
